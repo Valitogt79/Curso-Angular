bb6867fc01cf37e083932da847d54553c9ec6c24
Oswaldo Alonzo <oalonzo@gmail.com>
2025-02-26 10:30:55 -0600

    cambiando la importación del ejercicio 3

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,11 +1,10 @@
-import './style.css'
+import "./style.css";
 // import './topics/01-basic-types'   //-> requiriendo el modulo
+// import "./topics/02-object-interface.ts"; //-> requiriendo el modulo
+import "./topics/03-functions";
 
-document.querySelector<HTMLDivElement>('#app')!
+document.querySelector<HTMLDivElement>("#app")!;
 
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-
-
-app.innerHTML = `Hola Mundo!`
+app.innerHTML = `Hola Mundo!`;
