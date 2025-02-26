414cfbdd96132777b574dfbe454a4b5add1dfb8c
Oswaldo Alonzo <oalonzo@gmail.com>
2025-02-26 09:53:39 -0600

    Fix: indentación

--- 01-typescript-intro/src/topics/01-basic-types.ts
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -1,14 +1,9 @@
-const name: string = "Valitogt"
-let hpPoints: number | 'FUEL' = 95      //->permite utilizar valores numericos o string o directamente la cadena de texto
-const isAlive: boolean = true
+const name: string = "Valitogt";
+let hpPoints: number | "FUEL" = 95; //->permite utilizar valores numericos o string o directamente la cadena de texto
+const isAlive: boolean = true;
 
-hpPoints = 'FUEL'
+hpPoints = "FUEL";
 
 console.log({ name, hpPoints, isAlive });
 
-
-
-
-
-
-export { }
+export {};
