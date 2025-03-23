7d800fa761814bb8f5511602e487f8d7f4e26adf
Oswaldo Alonzo <oalonzo@gmail.com>
2025-03-23 08:34:04 -0600

    Add: estilos condicionales

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,11 +9,18 @@ interface Character {
 
 @Component({
   templateUrl: './dragonball-page.component.html',
+  // imports: [NgClass],
 })
 export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8001 },
-    { id: 3, name: 'Piccolo', power: 3001 },
+    { id: 2, name: 'Vegeta', power: 9000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
   ]);
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //   };
+  // });
 }
